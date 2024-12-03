d338c75fd764d5bb260cd58af67ce0bdb8831de9
chr1scr0sss <tiwakorn.matonboon@gmail.com>
2024-12-03 21:46:05 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int N;
+    int X = 0, Y = 0;
+while(N != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    if(N%2 == 0){
+        X++;
+    } else if(N%2 != 0){
+        Y++;
+    }
+}
+
+
+    cout << "#Even numbers = " << X-1 << endl;
+    cout << "#Odd numbers = " << Y << endl;
     return 0;
 }
+
